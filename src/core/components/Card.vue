7d72ea09277e7500cccb6f12666fe5c7ac71b108
Hanif Omar <hanif.omarr@gmail.com>
2024-10-27 19:55:22 +0800

    feat(ui): create custom card component

---
+++ src/core/components/Card.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props = defineProps({
+  class: { type: String, default: "" },
+});
+</script>
+
+<template>
+  <div :class="[props.class, 'bg-white rounded-xl shadow-md relative']">
+    <slot></slot>
+  </div>
+</template>
