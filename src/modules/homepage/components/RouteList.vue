eb874c31b4152b602c0f1e19ad5508ec319b1125
Hanif Omar <hanif.omarr@gmail.com>
2024-10-27 21:08:28 +0800

    feat(home): create homepage view

---
+++ src/modules/homepage/components/RouteList.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import Button from "@/core/components/Button.vue";
+import Card from "@/core/components/Card.vue";
+
+defineProps({
+  route: Object,
+});
+</script>
+
+<template>
+  <Card>
+    <div class="p-4">
+      <div class="mb-6">
+        <div class="text-gray-600 my-2">{{ route.location }}</div>
+        <h3 class="text-xl font-bold">{{ route.name }}</h3>
+      </div>
+
+      <div class="mb-5">
+        {{ route.description }}
+      </div>
+
+      <h3 class="text-green-500 mb-2">{{ route.distance_km }} KM</h3>
+
+      <div class="border border-gray-100 mb-5"></div>
+
+      <div class="flex flex-col lg:flex-row justify-between mb-4">
+        <div class="text-orange-700 mb-3">
+          <i class="fa-solid fa-location-dot text-lg"></i>
+          {{ route.difficulty }}
+        </div>
+        <RouterLink to="`/routes`">
+          <Button
+            variant="primary"
+            size="medium"
+            class="bg-green-500 hover:bg-green-600"
+            >See More</Button
+          >
+        </RouterLink>
+      </div>
+    </div>
+  </Card>
+</template>
---
+++ src/modules/homepage/views/HomePage.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { RouterLink } from "vue-router";
+
+import Hero from "../components/Hero.vue";
+import RouteList from "../components/RouteList.vue";
+import Button from "@/core/components/Button.vue";
+
+import routesData from "@/routes.json";
+
+const routes = ref(routesData);
+</script>
+
+<template>
+  <Hero />
+  <section class="bg-green-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Browse Running Route
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <RouteList v-for="route in routes" :key="route" :route="route" />
+      </div>
+    </div>
+  </section>
+  <section class="m-auto max-w-lg my-10 px-6">
+    <RouterLink to="/routes">
+      <Button class="w-full bg-black py-4 px-6 hover:bg-gray-700">
+        See More</Button
+      >
+    </RouterLink>
+  </section>
+</template>
